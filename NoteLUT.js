d1b1ae74213438fa13403d6aa6cc843a3edb64e7
skrywerbeer <liquidsquid1@gmail.com>
2026-04-07 14:39:47 +0200

    Refactored classes to separate files.

---
+++ NoteLUT.js
@@ -0,0 +1,57 @@
+export default class NoteLUT {
+  #NOTE_LUT;
+  constructor(count) {
+	this.NOTE_LUT = [... this.#lutGenerator(count)];
+  }
+
+  lookupFrequency(note) {
+	if (!("letter" in note)  || !("sharp" in note)  || !("octave" in note))
+	  throw new Error("Arugument error: expected object with .letter, .sharp and .octave defined");
+	if (note.letter == "p")
+	  return 0;
+	for (const element of this.NOTE_LUT) {
+	  if (element.note.letter === note.letter &&
+		  element.note.sharp === note.sharp &&
+		  element.note.octave === note.octave) {
+		return element.frequency;
+	  }
+	}
+	throw new Error(`Could not find ${note.letter}${note.sharp ? "#" : ""}${note.octave}.`);
+  }
+
+  *#noteGenerator() {
+	const notes =
+		[
+		  {letter: "a", sharp: false},
+		  {letter: "a", sharp: true},
+		  {letter: "b", sharp: false},
+		  {letter: "c", sharp: false},
+		  {letter: "c", sharp: true},
+		  {letter: "d", sharp: false},
+		  {letter: "d", sharp: true},
+		  {letter: "e", sharp: false},
+		  {letter: "f", sharp: false},
+		  {letter: "f", sharp: true},
+		  {letter: "g", sharp: false},
+		  {letter: "g", sharp: true}
+		];
+	for (let i = 0, octave = 0; ; i++) {
+	  if (i == notes.length) {
+		i = 0;
+		octave++;
+	  }
+
+	  yield {letter: notes[i].letter, sharp: notes[i].sharp, octave: octave}
+	}
+	return;
+  }
+
+  *#lutGenerator(count=12) {
+	let noteGen = this.#noteGenerator();
+	const a0Frequency = 27.5;
+	for (let i = 0; i < count; i++) {
+	  yield {note: noteGen.next().value, frequency: a0Frequency*Math.pow(2, i/12)};
+	}
+	return;
+  }
+}
---
+++ RTTTLParser.js
@@ -0,0 +1,98 @@
+import NoteLUT from "./NoteLUT.js";
+
+export default class RTTTLParser {
+  #NoteLUT;
+  constructor(parseString) {
+	this.parse(parseString);
+	this.#NoteLUT = new NoteLUT(100);
+  }
+  lut() {
+	return this.#NoteLUT;
+  }
+
+  parse(parseString) {
+	this.parsedString = parseString;
+	this.#parseSections();
+	this.#parseDefaultValues();
+	this.dataTokenIndex = 0;
+  }
+
+  *notes() {
+	for (const token of this.dataTokens)
+	  yield this.parseDataToken(token);
+	return;
+
+  }
+
+  parseDataToken(token) {
+	if (!token || typeof(token) != "string")
+	  throw new Error(`Argument error: expected string as argument for parseNote. Got: ${token}`);
+	const returnData = {};
+	returnData.letter = token?.match(/[abcdefgp]/g)?.at(0);
+	if (!returnData.letter)
+	  throw new Error(`Error parsing: ${token}. Expected one of {a, b, c, d, e, f, g, p}`);
+	const digits = token.match(/[0-9]*[0-9]/g) ?? [];
+
+	if (!isNaN(token[0])) { // If is a number.
+	  returnData.division = Number(digits.length > 0 ? digits[0] : this.defaultDivision);
+	  if (digits.length == 2) {
+		returnData.octave = Number(digits[1]);
+	  }
+	  else if (digits.length == 1) {
+		returnData.octave = this.defaultOctave;
+	  }
+	  // digits.length > 2
+	  else {
+		throw new Error(`Error parsing: ${token}. Expected only two digits.`);
+	  }
+	}
+	else {
+	  returnData.division = this.defaultDivision;
+	  if (digits.length == 1) {
+		returnData.octave = Number(digits[0]);
+	  }
+	  else if (digits.length == 0) {
+		returnData.octave = this.defaultOctave;
+	  }
+	  else {
+		throw new Error(`Error parsing note: ${token}`);
+	  }
+	}
+	returnData.sharp = token.match(/#/g) ? true : false;
+	returnData.dotted = token.match(/\./g) ? true : false;
+	returnData.frequency = this.#NoteLUT.lookupFrequency(
+		{
+		  letter: returnData.letter,
+		  octave: returnData.octave,
+		  sharp: returnData.sharp
+		});
+	returnData.duration = this.#bpmToMsPerBeat(this.bpm)/returnData.division;
+	if (returnData.dotted)
+	  returnData.duration *= 1.5;
+	return returnData;
+  }
+
+  #parseSections() {
+	let sections = this.parsedString.split(":");
+	if (sections.length != 3)
+	  throw new Error("Error parsing sections.");
+	this.name = sections[0];
+	this.defaultValuesTokens = sections[1].split(",");
+	this.defaultValuesTokens.forEach((element, index, array) => { array[index] = element.trim(); });
+	this.dataTokens = sections[2].split(",");
+	this.dataTokens.forEach((element, index, array) => { array[index] = element.trim(); });
+	// Some authors use a "," at the end.
+	if (this.dataTokens[this.dataTokens.length - 1] == "")
+	  this.dataTokens.pop();
+  }
+
+  #parseDefaultValues() {
+	// Use .slice(2) to throw away the *= parts.
+	this.defaultDivision = Number(this.defaultValuesTokens[0].slice(2));
+	this.defaultOctave = Number(this.defaultValuesTokens[1].slice(2));
+	this.bpm = Number(this.defaultValuesTokens[2].slice(2));
+  }
+  #bpmToMsPerBeat(bpm) {
+	return (60/bpm)*1000;
+  }
+}
